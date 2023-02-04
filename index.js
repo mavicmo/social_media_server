1167d8c0696bc73229b849b5afe70131a5d1ff8c
mtaraq5@gmail.com <mtaraq@Mos-MacBook-Pro.home>
2023-02-04 14:30:48 -0500

    post routes and controller

--- index.js
+++ index.js
@@ -13,7 +13,10 @@ import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
 import { register } from "./controllers/auth.js";
 import { createPost } from "./controllers/posts.js";
-import { verifyToken } from "./Middleware/auth";
+import { verifyToken } from "./Middleware/auth.js";
+import User from "./models/User.js";
+import Post from "./models/Post.js";
+import { users, posts } from "./data/index.js";
 
 /* CONFIGURATIONS  */
 const __filename = fileURLToPath(import.meta.url);
@@ -60,5 +63,7 @@ mongoose
   })
   .then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+    // User.insertMany(users);
+    // Post.insertMany(posts);
   })
   .catch((error) => console.log(`${error} did not connect`));
